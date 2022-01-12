a5b7682f116081b85baf401ac14cae9d2e1ed31c
minung.han <hmu332233@gmail.com>
2022-01-12 23:09:07 +0900

    feat: 카드 위치에 따라서 애니메이션 움직이는 정도 다르도록 변경

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,19 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 
 import dayjs from "dayjs";
-import 'animate.css';
-
-const getAnimationClass = (prevIndex: number, currentIndex: number): string => {
-  if (prevIndex === undefined) {
-    return 'animate__slideInLeft';
-  }
-
-  if (prevIndex === currentIndex) {
-    return '';
-  }
-
-  return prevIndex > currentIndex ? 'animate__slideInUp' : 'animate__slideInDown';
-};
+import Card from "./Card";
 
 function Home() {
   const [type, setType] = useState("goorm");
@@ -30,7 +18,7 @@ function Home() {
         
         setLectures(prevLectures => {
           prevOrderMap.current = prevLectures.reduce((prev, lecture, index) => ({ ...prev, [lecture.id]: index }), {});
-          return data.popular;
+          return data.latest;
         });
         
         
@@ -83,29 +71,7 @@ function Home() {
         </div>
         {date}
         {lectures.map((lecture, index) => (
-          <div className={`card shadow-lg compact side bg-base-100 mt-6 animate__animated ${getAnimationClass(prevOrderMap.current[lecture.id], index)}`} key={lecture.id}>
-            <div className="flex-row items-center space-x-5 card-body">
-              <div className="flex-0">
-                <span className="text-lg font-bold text-info">{index + 1}</span>
-              </div>
-              <div className="flex-1">
-                <h2 className="card-title text-lg">{lecture.title}</h2>
-                <p className="text-base-content text-opacity-40">
-                  {lecture.instructor || '-'}
-                </p>
-              </div>
-              <div className="flex-0">
-                <a
-                  className="btn btn-sm"
-                  href={lecture.url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Link
-                </a>
-              </div>
-            </div>
-          </div>
+          <Card lecture={lecture} index={index} prevOrderMap={prevOrderMap} />
         ))}
         {/* <div className="overflow-x-auto">
           <table className="table w-full">
