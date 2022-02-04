56d1d72df3bf9cc53713e10befd1092034d669d0
minung.han <hmu332233@gmail.com>
2022-02-05 03:09:38 +0900

    refactor: runner type 정리

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,46 @@ import axios, { AxiosResponse } from 'axios';
 import { createHistoryFileAndPush } from './utils/git';
 // import { createHistoryIssue } from './utils/github';
 
-const createLectures = async (fetch: Promise<AxiosResponse<any, any>>, converter: (data: any) => Lecture[]) => {
+type Converter = (data: any) => Lecture[];
+type RunnerMap = {
+  [key: string]: {
+    apiUrl: string,
+    converter: Converter,
+  }
+}
+
+const runnerMap: RunnerMap = {
+  goorm: {
+    apiUrl: 'https://edu.goorm.io/api/lecture/list/detail?limit=20&page=1&sort=newest',
+    converter: ({ list }) => (
+      list.map((item: any) => {
+        return {
+          title: item.subject,
+          sequence: item.sequence,
+          url: `https://edu.goorm.io/lecture/${item.sequence}/${item.url_slug}`,
+          image: item.coverImage,
+        };
+      })
+    )
+  }
+}
+
+
+const createLectures = async (fetch: Promise<AxiosResponse<any, any>>, converter: Converter) => {
   const { data } = await fetch;
   return converter(data);
 };
 
 async function run() {
-    const lectures: Lecture[] = await createLectures(
-      axios.get('https://edu.goorm.io/api/lecture/list/detail?limit=20&page=1&sort=newest'),
-      ({ list }) => (
-        list.map((l: any) => {
-          return {
-            title: l.subject,
-            sequence: l.sequence,
-            url: `https://edu.goorm.io/lecture/${l.sequence}/${l.url_slug}`,
-            image: l.coverImage
-          };
-        })
-      )
-    );
-    
-    createHistoryFileAndPush('goorm', lectures);
-    // await createHistoryIssue('goorm', lectures);
+  const key = 'goorm';
+  const { apiUrl, converter } = runnerMap[key];
+  const lectures: Lecture[] = await createLectures(
+    axios.get(apiUrl),
+    converter,
+  );
+
+  createHistoryFileAndPush('goorm', lectures);
+  // await createHistoryIssue('goorm', lectures);
 }
 
 run();
