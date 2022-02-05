48ffb13b3675aebcb1e0945609318490f40c3ffc
Minung Han <hmu332233@gmail.com>
2022-02-06 03:38:28 +0900

    feat: nextjs api 설정 및 테스트

--- site/next.config.js
+++ site/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/lectures/:comp/:date',
+        destination: `https://raw.githubusercontent.com/hmu332233/action.new-lecture/main/history/:comp/:date.json`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
