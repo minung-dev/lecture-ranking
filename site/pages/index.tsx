28b392b5df2c8b6b5ecab6cb5fa01c4d6c99a4a3
Minung Han <hmu332233@gmail.com>
2022-02-08 00:27:04 +0900

    feat: type, date 변경 기능으로 기본 mvp 기능 추가

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,53 +1,81 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import { useEffect, useState } from "react";
-import styles from "../styles/Home.module.css";
 
-import dayjs from 'dayjs';
+import dayjs from "dayjs";
 
 function Home() {
-  const [date, setDate] = useState(dayjs().format('YYYY-MM-DD'));
+  const [type, setType] = useState('goorm')
+  const [date, setDate] = useState(dayjs().add(-1, "day").format("YYYY-MM-DD"));
   const [lectures, setLectures] = useState([]);
   useEffect(() => {
     (async () => {
-      const data = await fetch(`/api/lectures/inflearn/${date}`).then((res) =>
-        res.json()
-      );
-      setLectures(data.popular);
+      try {
+        const data = await fetch(`/api/lectures/${type}/${date}`).then((res) =>
+          res.json()
+        );
+        setLectures(data.popular);
+      } catch (err) {
+        console.log(err);
+      }
     })();
-  }, [date]);
+  }, [type, date]);
 
   const handlePrev = () => {
-    const prevDate = dayjs(date).add(-1, 'day').format('YYYY-MM-DD');
+    const prevDate = dayjs(date).add(-1, "day").format("YYYY-MM-DD");
     setDate(prevDate);
-  }
+  };
+
+  const handleNext = () => {
+    const prevDate = dayjs(date).add(1, "day").format("YYYY-MM-DD");
+    setDate(prevDate);
+  };
+
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { currentTarget: { value } } = e;
+    setType(value);
+  };
 
   return (
-    <div>
-      <div className="btn-group">
-        <button className="btn btn-outline btn-sm btn-wide" onClick={handlePrev}>이전</button> 
-        <button className="btn btn-outline btn-sm btn-wide">다음</button>
-      </div>
-      <div className="overflow-x-auto">
-        <table className="table w-full">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Link</th>
-            </tr>
-          </thead>
-          <tbody>
-            {lectures.map((lecture, index) => (
-              <tr key={lecture.id}>
-                <th>{index}</th>
-                <td>{lecture.title}</td>
-                <td>Link</td>
+    <div className="mockup-window bg-base-300">
+      <div className="flex flex-col justify-center px-4 py-16 bg-base-200">
+        <select className="select select-bordered w-full max-w-xs" onChange={handleSelect}>
+          <option value="goorm">goorm</option>
+          <option value="inflearn">inflearn</option>
+        </select>
+        <div className="btn-group">
+          <button
+            className="btn btn-outline btn-sm btn-wide"
+            onClick={handlePrev}
+          >
+            이전
+          </button>
+          <button
+            className="btn btn-outline btn-sm btn-wide"
+            onClick={handleNext}
+          >
+            다음
+          </button>
+        </div>
+        {date}
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Link</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {lectures.map((lecture, index) => (
+                <tr key={lecture.id}>
+                  <th>{index}</th>
+                  <td>{lecture.title}</td>
+                  <td>Link</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
