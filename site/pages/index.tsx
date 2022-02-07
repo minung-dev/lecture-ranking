b7523f9896008aeac7dcb2c9e58a78b9de5cff0d
Minung Han <hmu332233@gmail.com>
2022-02-07 22:50:04 +0900

    chore: daisyUI 추가

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,27 +1,42 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
 
 function Home() {
   const [lectures, setLectures] = useState([]);
   useEffect(() => {
     (async () => {
-      const data = await fetch('/api/lectures/goorm/2022-02-05').then(res => res.json());
-      setLectures(data.popularPaid)
+      const data = await fetch("/api/lectures/goorm/2022-02-05").then((res) =>
+        res.json()
+      );
+      setLectures(data.popularPaid);
     })();
   }, []);
 
   return (
-    <div>
-      {lectures.map(lecture => (
-        <div key={lecture.id}>
-          <h4>{lecture.title}</h4>
-        </div>
-      ))}
+    <div className="overflow-x-auto">
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {lectures.map((lecture, index) => (
+            <tr key={lecture.id}>
+              <th>{index}</th>
+              <td>{lecture.title}</td>
+              <td>Link</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
 export default Home;
