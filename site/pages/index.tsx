f0eb66732a8b55566b720ac9da62b29ff7bf5ca1
Minung Han <hmu332233@gmail.com>
2022-02-07 23:11:44 +0900

    feat: dayjs를 이용해서 시간 변경시 테이블 변경되도록 수정

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -4,37 +4,51 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
+import dayjs from 'dayjs';
+
 function Home() {
+  const [date, setDate] = useState(dayjs().format('YYYY-MM-DD'));
   const [lectures, setLectures] = useState([]);
   useEffect(() => {
     (async () => {
-      const data = await fetch("/api/lectures/goorm/2022-02-05").then((res) =>
+      const data = await fetch(`/api/lectures/inflearn/${date}`).then((res) =>
         res.json()
       );
-      setLectures(data.popularPaid);
+      setLectures(data.popular);
     })();
-  }, []);
+  }, [date]);
+
+  const handlePrev = () => {
+    const prevDate = dayjs(date).add(-1, 'day').format('YYYY-MM-DD');
+    setDate(prevDate);
+  }
 
   return (
-    <div className="overflow-x-auto">
-      <table className="table w-full">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Link</th>
-          </tr>
-        </thead>
-        <tbody>
-          {lectures.map((lecture, index) => (
-            <tr key={lecture.id}>
-              <th>{index}</th>
-              <td>{lecture.title}</td>
-              <td>Link</td>
+    <div>
+      <div className="btn-group">
+        <button className="btn btn-outline btn-sm btn-wide" onClick={handlePrev}>이전</button> 
+        <button className="btn btn-outline btn-sm btn-wide">다음</button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Link</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {lectures.map((lecture, index) => (
+              <tr key={lecture.id}>
+                <th>{index}</th>
+                <td>{lecture.title}</td>
+                <td>Link</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
