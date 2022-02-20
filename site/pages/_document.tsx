41e9e211223122a61b60479ab7eadde5881eacb2
minung.han <hmu332233@gmail.com>
2022-02-20 18:43:00 +0900

    ga 추가

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -3,7 +3,24 @@ import { Html, Head, Main, NextScript } from 'next/document';
 function Document() {
   return (
     <Html data-theme="dark">
-      <Head />
+      <Head>
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-64LNLP2ET9"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-64LNLP2ET9');
+            `,
+          }}
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
