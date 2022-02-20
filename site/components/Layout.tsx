c532fbb831a4db1b29d7ce138ffee44d190977d9
minung.han <hmu332233@gmail.com>
2022-02-20 19:29:03 +0900

    파비콘 설정

--- site/components/Layout.tsx
+++ site/components/Layout.tsx
@@ -1,12 +1,19 @@
+import Head from 'next/head';
+
 type Props = {
   children: JSX.Element[] | JSX.Element;
 };
 
 function Layout({ children }: Props) {
   return (
-    <div className="flex flex-col justify-center pt-5 border-t border-base-300 bg-base-200 border border-base-200 rounded-2xl">
-      {children}
-    </div>
+    <>
+      <Head>
+        <title>Lecture Ranking!</title>
+      </Head>
+      <div className="flex flex-col justify-center pt-5 border-t border-base-300 bg-base-200 border border-base-200 rounded-2xl">
+        {children}
+      </div>
+    </>
   );
 }
 
